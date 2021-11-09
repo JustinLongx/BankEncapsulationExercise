0fb34d124fb74d980d01eb07cbd6fc276be21d53
justin long <justinxlong93@gmail.com>
2021-11-09 17:29:47 -0600

    Initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"simulating a deposit of {amount.ToString("C")} to your account! ");
+            _balance = amount;
+        }
+
+        public double GetBalance() 
+        {
+            return _balance;
+        }
+        
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bAccount = new BankAccount();
+
+            Console.WriteLine("Welcome to BOSS Bank, How much money do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            bAccount.Deposit(amountToDeposit);
+            double userBalance = bAccount.GetBalance();
+            Console.WriteLine($"Your current balance is: ${userBalance} ");
+        }
+    }
+}
